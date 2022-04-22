ca491952a96c78f1859d0b15493b91edac1209f6
ZKillou <55600416+ZKillou@users.noreply.github.com>
2022-04-22 22:56:13 +0200

    Cool command and event handler i made (#5)

---
+++ aria/commands/queue.js
@@ -0,0 +1,32 @@
+const {
+  Formatters: { hyperlink, time },
+  MessageEmbed
+} = require('discord.js');
+
+module.exports = {
+  name: "queue",
+  isTeamOnly: true,
+  async execute(client, message, args) {
+    // the queue system
+    // BOT NAME (PREFIX) - INVITE (NO PERMS) - INVITE (8 PERMS)
+    // if its apprvoed dont show it on the list
+
+    const embed = new MessageEmbed()
+      .setTitle('***Queue***')
+      .setDescription(
+        Object.values(client.DB)
+          .filter((bot) => !bot.approved)
+          .map((bot) => {
+            const invite = `https://discord.com/oauth2/authorize?client_id=${bot.id}&scope=bot%20applications.commands&perms=`;
+
+            return `${bot.name} (${bot.prefix}) - ${hyperlink(
+              'invite (no perms)',
+              `${invite}0`
+            )} - ${hyperlink('invite (administrator perms)', `${invite}8`)}`;
+          })
+          .join('\n\n')
+      );
+
+    await message.channel.send({ embeds: [embed] });
+  }
+};
---
+++ aria/events/ready.js
@@ -0,0 +1,6 @@
+module.exports = {
+  once: true,
+  execute(client){
+    console.log(`${client.user.tag} is online`)
+  }
+}
