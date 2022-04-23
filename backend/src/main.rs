a88c87a180ca8e31dac4fb7d954e1f78fcaef761
Ashley <iamashley@tuta.io>
2022-04-23 15:22:25 +0300

    Update main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 use hyper::Server;
 use hyper::{Request, Response};
 use hyper::service::service_fn;
@@ -28,25 +27,4 @@ async fn main() {
         eprintln!("server error: {}", e);
     }
 }
-=======
-/*
-    This Source Code Form is subject to the terms of the GNU General Public License:
-    Copyright (C) 2021-2022 OPENBOTLIST CONTRUBUTORS 
-    
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-     
-    You should have received a copy of the GNU General Public License
-    along with this program. If not, see https://www.gnu.org/licenses/.
-*/
-fn main() {
-    println!("this is the backend of the website,still in develoment! looking for rust devs to help us make it real!!!!");
-}
->>>>>>> 2d9a4aa17e5914224968f944a5673a664290e643
+ 
