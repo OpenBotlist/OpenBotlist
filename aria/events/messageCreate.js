bc3ea4883bd6fc14de066dd33c308e98cdcec5b7
Dennis <43997085+TheAlan404@users.noreply.github.com>
2022-04-24 12:07:44 +0300

    always delete messages in add bot channel

--- aria/events/messageCreate.js
+++ aria/events/messageCreate.js
@@ -1,41 +1,42 @@
 module.exports = {
-  async execute(client, message){
-    console.log(message)
-    if (message.author.bot) return;
-	  if (message.channel.type === "DM") return;
-    if (!message.content.toLowerCase().startsWith(client.botoptions.prefix)) return;
+	async execute(client, message){
+		//console.log(message) // who did this? -dennis
+		if (message.author.bot) return;
+		if (message.channel.type === "DM") return;
+		if (message.channel.id === client.botoptions.add_bot && !message.content.startsWith("!add-bot")) await message.delete();
+		if (!message.content.toLowerCase().startsWith(client.botoptions.prefix)) return;
 
-    console.log("ok 1")
+		console.log("ok 1")
 
-    const args = message.content.slice(client.botoptions.prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
-    const command = client.commands.get(commandName);
+		const args = message.content.slice(client.botoptions.prefix.length).split(/ +/);
+		const commandName = args.shift().toLowerCase();
+		const command = client.commands.get(commandName);
 
-    console.log("ok 2", command)
+		console.log("ok 2", command)
 
-    if(!command) return;
+		if(!command) return;
 
-    if(command.isTeamOnly && !client.botoptions.team.includes(message.author.id)){
-      await message.channel.send('You have to be in the team');
-      return;
-    };
+		if(command.isTeamOnly && !client.botoptions.team.includes(message.author.id)){
+			await message.channel.send('You have to be in the team');
+			return;
+		};
 
-    if(command.isBottumReviewerOnly && !client.botoptions.bottumrev.includes(message.author.id)){
-      await message.channel.send('You have to be a bottum reviewer bruh');
-      return;
-    };
+		if(command.isBottumReviewerOnly && !client.botoptions.bottumrev.includes(message.author.id)){
+			await message.channel.send('You have to be a bottum reviewer bruh');
+			return;
+		};
 
-    if(command.isAddBotChannelOnly && message.channel.id != client.botoptions.add_bot) {
-      await message.channel.send(
-        'oh god ur cringe use this command at <#966397896176058428> smh ppl these days'
-      );
-      return;
-    };
+		if(command.isAddBotChannelOnly && message.channel.id != client.botoptions.add_bot) {
+			await message.channel.send(
+			'oh god ur cringe use this command at <#966397896176058428> smh ppl these days'
+			);
+			return;
+		};
 
-    try {
-      await command.execute(client, message, args);
-    } catch(error) {
-      await message.channel.send(`An error occured ...\n\`\`\`xl\n${error}\`\`\``);
-    };
-  }
+		try {
+			await command.execute(client, message, args);
+		} catch(error) {
+			await message.channel.send(`An error occured ...\n\`\`\`xl\n${error}\`\`\``);
+		};
+	}
 }
