c70416460e931aa1a26a09e768bed841aa49a9c3
Ashley <iamashley@tuta.io>
2022-04-24 13:02:55 +0300

    added __dirname

--- aria/util/loaders.js
+++ aria/util/loaders.js
@@ -6,24 +6,23 @@ const { Client } = require("discord.js");
  * @param {Client} client 
  * @param {String} dir 
  */
-const loadCommands = (client, dir = "./commands") => {
+const loadCommands = (client, dir = require("path").join(__dirname, "../commands")) => {
   const files = readdirSync(dir).filter(f => f.endsWith(".js"));
   for(const file of files){
-    const command = require(`../${dir}/${file}`);
+    const command = require(`${dir}/${file}`);
     client.commands.set(command.name, command);
     console.log(`The command ${command.name} is loaded :3`);
-  };
-};
-
+  }
+}
 /**
  * Load all the events
  * @param {Client} client 
  * @param {String} dir 
  */
-const loadEvents = (client, dir = "./events") => {
+const loadEvents = (client, dir = require("path").join(__dirname, "../events")) => {
   const files = readdirSync(dir).filter(f => f.endsWith(".js"));
   for(const file of files){
-    const event = require(`../${dir}/${file}`);
+   const event = require(`${dir}/${file}`);
     const eventName = file.split(".")[0];
 
     if(event.once) client.once(eventName, event.execute.bind(null, client));
